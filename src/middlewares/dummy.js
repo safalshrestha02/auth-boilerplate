88c96479722045a2ee7b3dc0e7a61feed22e4e95
Magiciaan <surajkhadka315@gmail.com>
2023-03-23 13:59:25 +0545

    roles check

--- src/middlewares/dummy.js
+++ src/middlewares/dummy.js
@@ -1,1 +1,16 @@
 // dummydummy
+const User = require("../models/User");
+
+exports.checkUserRole = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is unauthorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
