1796ed418c3859a49e756fd825ab1597113ef11c
Safal <Safal Shrestha>
2023-03-28 17:04:49 +0545

    feat: code clean

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,16 +1,11 @@
 require("dotenv").config();
 
 const allowedOrigins = [
-<<<<<<< HEAD
   `http://localhost:/${process.env.PORT}`, // your localhost url
   "https://www.yourWebsite.com", // your domain url
   "https://www.google.com",
   "https://accounts.google.com/",
-  "https://play.google.com/"
-=======
-  `http://localhost:${process.env.PORT}`, // your localhost url
-  "https://www.yourWebsite.com", // your domain url
->>>>>>> 0a919f6 (feat: email verification)
+  "https://play.google.com/",
 ];
 
 module.exports = allowedOrigins;
